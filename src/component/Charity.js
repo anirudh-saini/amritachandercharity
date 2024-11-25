558a0146e72274d0d7bdbe4670367d39dac6a30f
saini-baba <anirudh.saini.smartdata@gmail.com>
2024-11-25 12:00:52 +0530

    charity done

---
+++ src/component/Charity.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./charity.module.scss";
+export const Charity = () => {
+  return (
+    <div className={styles.charity}>
+      <div className="container">
+        <div className={styles.make}>
+          <h2>MAKE YOUR OWN DAY OF CHARITY</h2>
+          <p>
+            Add goodwill to your special occasions by spreading joy among those
+            who need your care and support the most.
+          </p>
+        </div>
+        <div className={styles.choose}>
+          <div>
+            <p>CHOOSE AN OCCASION</p>
+          </div>
+          <div>
+            <div>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLScjc6uaaQO15us5v00RfzGLWcbLhQ8WP5zxMwGe-BJkoCOSDg/viewform"
+                target="_black"
+              >
+                <img src="celebration.png" alt="" />
+                <p>Celebration</p>
+              </a>
+            </div>
+            <div>
+              <img src="inHonorof.png" alt="" />
+              <p>In honour of</p>
+            </div>
+            <div>
+              <img src="inMemoryof.png" alt="" />
+              <p>In memory of</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,18 @@ import styles from "./home.module.scss";
 import { Hero } from "../component/Hero";
 import { Donate } from "../component/Donate";
 import { Carousel } from "../component/Carousel";
+import { Readmore } from "../component/Readmore";
+import { Vision } from "../component/Vision";
+import { Charity } from "../component/Charity";
 export const Home = () => {
-    return (
-        <div>
-            <Hero />
-            <Donate />
-            <Carousel />
-        </div>
-    );
+  return (
+    <div>
+      <Hero />
+      <Donate />
+      <Carousel />
+      <Readmore />
+      <Vision />
+      <Charity />
+    </div>
+  );
 };
