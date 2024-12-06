6de020c8a7881180183bd90a4f9c60a56257dc82
saini-baba <anirudh.saini.smartdata@gmail.com>
2024-12-06 12:32:27 +0530

    nav changed

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState ,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./nav.module.scss";
 export const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,26 +21,50 @@ export const Nav = () => {
         <div className={`container ${styles.social}`}>
           <div>
             <a href="tel:+91 1144753285">
-              <img src="http://localhost:3000/phone.svg" alt="phone" />
+              <img
+                src="http://localhost:3000/phone.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/phone.svg';"
+                alt="phone"
+              />
               <p>+91 1144753285</p>
             </a>
             <a href="mailto:mail@amritachandercharity.org.in">
-              <img src="http://localhost:3000/email.svg" alt="mail" />
+              <img
+                src="http://localhost:3000/email.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/email.svg';"
+                alt="mail"
+              />
               <p>mail@amritachandercharity.org.in</p>
             </a>
           </div>
           <div>
             <a href="/">
-              <img src="http://localhost:3000/insta.svg" alt="social" />
+              <img
+                src="http://localhost:3000/insta.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/insta.svg';"
+                alt="social"
+              />
             </a>
             <a href="/">
-              <img src="http://localhost:3000/linkedin.svg" alt="social" />
+              <img
+                src="http://localhost:3000/linkedin.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/linkedin.svg';"
+                alt="social"
+              />
             </a>
             <a href="/">
-              <img src="http://localhost:3000/facebook.svg" alt="social" />
+              <img
+                src="http://localhost:3000/facebook.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/facebook.svg';"
+                alt="social"
+              />
             </a>
             <a href="/">
-              <img src="http://localhost:3000/youtube.svg" alt="social" />
+              <img
+                src="http://localhost:3000/youtube.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/youtube.svg';"
+                alt="social"
+              />
             </a>
           </div>
         </div>
@@ -49,7 +73,11 @@ export const Nav = () => {
         <nav className={`container ${styles.navbar}`}>
           <div className={styles.navbar_logo}>
             <a href="/">
-              <img src="http://localhost:3000/logo.svg" alt="logo" />
+              <img
+                src="http://localhost:3000/logo.svg"
+                onError="this.onerror=null; this.src='http://13.127.143.155/logo.svg';"
+                alt="logo"
+              />
             </a>
           </div>
           <div className={styles.hamburger} onClick={toggleMenu}>
@@ -83,29 +111,40 @@ export const Nav = () => {
                 <div className={styles.media}>
                   <div>
                     <a href="/">
-                      <img src="http://localhost:3000/insta.svg" alt="social" />
-                    </a>
-                    <a href="/">
                       <img
-                        src="http://localhost:3000/linkedin.svg"
+                        src="http://localhost:3000/insta_nav.svg"
+                        onError="this.onerror=null; this.src='http://13.127.143.155/insta_nav.svg';"
                         alt="social"
                       />
                     </a>
                     <a href="/">
                       <img
-                        src="http://localhost:3000/facebook.svg"
+                        src="http://localhost:3000/linkedin_nav.svg"
+                        onError="this.onerror=null; this.src='http://13.127.143.155/linkedin_nav.svg';"
                         alt="social"
                       />
                     </a>
                     <a href="/">
                       <img
-                        src="http://localhost:3000/youtube.svg"
+                        src="http://localhost:3000/facebook_nav.svg"
+                        onError="this.onerror=null; this.src='http://13.127.143.155/facebook_nav.svg';"
+                        alt="social"
+                      />
+                    </a>
+                    <a href="/">
+                      <img
+                        src="http://localhost:3000/youtube_nav.svg"
+                        onError="this.onerror=null; this.src='http://13.127.143.155/youtube_nav.svg';"
                         alt="social"
                       />
                     </a>
                   </div>
                   <div>
-                    <img src="http://localhost:3000/logo.svg" alt="logo" />
+                    <img
+                      src="http://localhost:3000/logo.svg"
+                      onError="this.onerror=null; this.src='http://13.127.143.155/logo.svg';"
+                      alt="logo"
+                    />
                   </div>
                 </div>
               </li>
